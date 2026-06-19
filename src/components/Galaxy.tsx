7aba884e3648019761c90408e962e42accfd1e7c
Daniel Bertol <daniel@fractalsys.com>
2026-06-19 17:44:26 -0300

    style: increase galaxy star density by 30%

--- src/components/Galaxy.tsx
+++ src/components/Galaxy.tsx
@@ -199,7 +199,7 @@ export default function Galaxy({
   focal = [0.5, 0.5],
   rotation = [1.0, 0.0],
   starSpeed = 0.5,
-  density = 1,
+  density = 1.3,
   hueShift = 140,
   disableAnimation = false,
   speed = 1.0,
